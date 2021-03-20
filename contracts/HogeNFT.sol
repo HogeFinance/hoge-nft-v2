6a987872596ce1252b95a6ad14cf20952527be2a
hogeman-hoges <rritters1776@yahoo.com>
2021-03-20 17:33:11 -0400

    Fixing 1st generation bug and updating tests. Uris assign correctly now

--- contracts/HogeNFT.sol
+++ contracts/HogeNFT.sol
@@ -6,10 +6,8 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721Pausable.sol";
 
-contract HogeNFT is Context, AccessControl, ERC721Burnable, ERC721Pausable {
+contract HogeNFT is Context, AccessControl, ERC721 {
     using Counters for Counters.Counter;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -18,6 +16,8 @@ contract HogeNFT is Context, AccessControl, ERC721Burnable, ERC721Pausable {
     Counters.Counter private _tokenIdTracker;
 
     event Received(address, uint);
+    event Mint(address from, address to, string uri);
+
     receive() external payable {
         emit Received(msg.sender, msg.value);
     }
@@ -32,7 +32,8 @@ contract HogeNFT is Context, AccessControl, ERC721Burnable, ERC721Pausable {
     }
 
     function mint(address to, string memory uri) public returns (uint) {
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have minter role to mint");
+        emit Mint(_msgSender(), to, uri);
+        require(hasRole(MINTER_ROLE, _msgSender()), "Must have minter role to mint");
 
         _safeMint(to, _tokenIdTracker.current());
         _setTokenURI(_tokenIdTracker.current(), uri);
@@ -40,17 +41,11 @@ contract HogeNFT is Context, AccessControl, ERC721Burnable, ERC721Pausable {
     }
 
     function mintBatch(address[] memory recipients, bytes32[] memory uri_bytes) public virtual {
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauser: must have minter role to mint");
-
-        string[] storage uris;
-
-        for (uint256 i = 0; i < uri_bytes.length; i++) {
-            uris.push(bytes32ToString(uri_bytes[i]));
-        }
+        require(hasRole(MINTER_ROLE, _msgSender()), "Must have minter role to mint");
 
         for (uint256 i = 0; i < recipients.length; i++) {
             _safeMint(recipients[i], _tokenIdTracker.current());
-            _setTokenURI(_tokenIdTracker.current(), uris[i]);
+            _setTokenURI(_tokenIdTracker.current(), bytes32ToString(uri_bytes[i]));
             _tokenIdTracker.increment();
         }
     }
@@ -72,17 +67,12 @@ contract HogeNFT is Context, AccessControl, ERC721Burnable, ERC721Pausable {
         return string(bytesStringTrimmed);
     }
 
-    function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have pauser role to pause");
-        _pause();
+    function burn(uint256 tokenId) public virtual {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "Burn: Caller is not owner nor approved");
+        _burn(tokenId);
     }
 
-    function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have pauser role to unpause");
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Pausable) {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 }
--- test/TestHogeNFT.js
+++ test/TestHogeNFT.js
@@ -1,13 +1,12 @@
-// test/Box.test.js
 // Load dependencies
 const { expect } = require('chai');
+const { BN, expectEvent, expectRevert, constants } = require('@openzeppelin/test-helpers');
+const HogeNFT = artifacts.require('HogeNFT');
+
 require('dotenv').config();
+require('web3');
 
 // Import utilities from Test Helpers
-const { BN, expectEvent, expectRevert, constants } = require('@openzeppelin/test-helpers');
-
-// Load compiled artifacts
-const HogeNFT = artifacts.require('HogeNFT');
 
 // Start test block
 contract('HogeNFT', function ([ creator, other, holder ]) {
@@ -18,7 +17,6 @@ contract('HogeNFT', function ([ creator, other, holder ]) {
     const name = 'HogeNFT';
     const symbol = 'NFT';
     const baseURI = 'https://example.com/token/';
-
 
     beforeEach(async function () {
         this.token = await HogeNFT.new(name, symbol, baseURI, { from: creator });
@@ -32,20 +30,42 @@ contract('HogeNFT', function ([ creator, other, holder ]) {
 
     it('minter can mint', async function () {
         const tokenIdZero = new BN('0');
+        const someURI = 'foo';
 
-        const receipt = await this.token.mint(holder, { from: creator });
+        const receipt = await this.token.mint(holder, someURI, { from: creator });
 
         expect(await this.token.ownerOf(tokenIdZero)).to.be.equal(holder);
-        expect(await this.token.tokenURI(tokenIdZero)).to.be.equal(`${baseURI}${tokenIdZero}`);
+        expect(await this.token.tokenURI(tokenIdZero)).to.be.equal(`${baseURI}${someURI}`);
         expectEvent(receipt, 'Transfer', { from: constants.ZERO_ADDRESS, to: holder, tokenId: tokenIdZero });
     });
 
     it('non minter cannot mint', async function () {
         // Test a transaction reverts
         await expectRevert(
-            this.token.mint(holder, { from: other }),
-            'ERC721PresetMinterPauserAutoId: must have minter role to mint.'
+            this.token.mint(holder, 'bar', { from: other }),
+            'Must have minter role to mint -- Reason given: Must have minter role to mint.'
         );
+    });
+
+    it('mints batches', async function() {
+        const uris = [web3.utils.fromAscii("foo"), web3.utils.fromAscii("bar")];
+        const recipients = [creator, holder];
+
+        const receipt = await this.token.mintBatch(recipients, uris, { from: creator });
+
+        expect(await this.token.ownerOf(new BN('0'))).to.be.equal(creator);
+        expect(await this.token.ownerOf(new BN('1'))).to.be.equal(holder);
+    });
+
+    it ('correctly assigns uris', async function() {
+        const uri1 = web3.utils.asciiToHex("foo");
+        const uri2 = web3.utils.asciiToHex("bar");
+
+        const receipt  = await this.token.mintBatch([holder], [uri1], { from: creator }); // Token 0
+        const receipt2 = await this.token.mintBatch([holder], [uri2], { from: creator }); // Token 1
+
+        expect(await this.token.tokenURI(new BN('0'))).to.be.equal(baseURI + web3.utils.hexToAscii(uri1));
+        expect(await this.token.tokenURI(new BN('1'))).to.be.equal(baseURI + web3.utils.hexToAscii(uri2));
     });
 
     // This is a convenience method.
